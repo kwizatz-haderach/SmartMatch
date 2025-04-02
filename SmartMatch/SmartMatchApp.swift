8364b1711f3b594c1dc106daeb0d0f0f0d47d182
BCE <barkancanerdogdu@gmail.com>
2025-04-02 14:01:26 +0300

    hello world

---
+++ SmartMatch/SmartMatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  SmartMatchApp.swift
+//  SmartMatch
+//
+//  Created by BCE on 2.04.2025.
+//
+
+import SwiftUI
+
+@main
+struct SmartMatchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
