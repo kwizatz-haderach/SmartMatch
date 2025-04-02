4f4a1e8d2a4a6a3cd4132f99ff2ed4341f9fa98a
BCE <barkancanerdogdu@gmail.com>
2025-04-02 14:33:31 +0300

    add LoginView

--- SmartMatch/LoginView.swift
+++ SmartMatch/LoginView.swift
@@ -9,6 +9,37 @@ import SwiftUI
 
 struct LoginView: View {
     
+
+    
+    var body: some View {
+        VStack(spacing: 20) {
+            Text("Login to SmartMatch")
+                .font(.title)
+                .bold()
+
+            TextField("Email", text: .constant(""))
+                .textFieldStyle(RoundedBorderTextFieldStyle())
+                .padding(.horizontal)
+                .autocapitalization(.none)
+
+            SecureField("Password", text: .constant(""))
+                .textFieldStyle(RoundedBorderTextFieldStyle())
+                .padding(.horizontal)
+
+            Button(action: {
+                print("Login tapped")
+            }) {
+                Text("Login")
+                    .frame(maxWidth: .infinity)
+                    .padding()
+                    .background(Color.blue)
+                    .foregroundColor(.white)
+                    .cornerRadius(10)
+                    .padding(.horizontal)
+            }
+        }
+        .padding()
+    }
     
 }
 
